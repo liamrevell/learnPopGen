930d82b5954e310ec6641d0a02fa2a92d4fe81f7
Liam J. Revell <liam.revell@umb.edu>
2018-03-06 19:25:06 -0500

    new function coalescent.plot

--- R/evolution.R
+++ R/evolution.R
@@ -26,6 +26,9 @@ hardy.weinberg<-function(p=c(0.5,0.5),alleles=c("A","a"),as.matrix=FALSE){
 	}
 }
 
+## compute multilocus Hardy-Weinberg in which each individual locus is biallelic
+## written by Liam J. Revell 2018
+
 multilocus.hw<-function(nloci=2,p=NULL){
 	if(is.null(p)) p<-rep(0.5,nloci)
 	if(length(p)!=nloci) nloci<-length(p)
@@ -108,6 +111,42 @@ phenotype.selection<-function(nloci=6,p=NULL,effect=1/nloci,beta=0.1,ngen=20,...
 		pp<-vector()
 		for(j in 1:nloci) pp[j]<-sum(FREQp[het[,j]]/2)+sum(FREQp[homo[,j]])
 		p<-pp
+		Sys.sleep(sleep)
+	}
+}
+
+## plot a coalescent genealogy
+## written by Liam J. Revell 2018
+
+coalescent.plot<-function(n=10,ngen=20,colors=NULL,...){
+	if(hasArg(sleep)) sleep<-list(...)$sleep
+	else sleep<-0.2
+	if(is.null(colors)) colors<-rainbow(n=n)
+	popn<-matrix(NA,ngen+1,n)
+	parent<-matrix(NA,ngen,n)
+	popn[1,]<-1:n
+	for(i in 1:ngen){
+		parent[i,]<-sort(sample(1:n,replace=TRUE))
+		popn[i+1,]<-popn[i,parent[i,]]
+	}
+	plot.new()
+	par(mar=c(2.1,4.1,2.1,1.1))
+	plot.window(xlim=c(0.5,n+0.5),ylim=c(ngen,0))
+	axis(2)
+	title(ylab="time (generations)")
+	cx.pt<-2*25/max(n,ngen)
+	points(1:n,rep(0,n),bg=colors,pch=21,cex=cx.pt)
+	for(i in 1:ngen){
+		dev.hold()
+		for(j in 1:n){
+			lines(c(parent[i,j],j),c(i-1,i),lwd=2,
+				col=colors[popn[i+1,j]])
+		}
+		points(1:n,rep(i-1,n),bg=colors[popn[i,]],pch=21,
+			cex=cx.pt)
+		points(1:n,rep(i,n),bg=colors[popn[i+1,]],pch=21,
+			cex=cx.pt)
+		dev.flush()
 		Sys.sleep(sleep)
 	}
 }
