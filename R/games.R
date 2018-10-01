ad1099542520df5e050c12ddd30320aae3a077d4
Liam J. Revell <liam.revell@umb.edu>
2018-10-01 12:52:10 -0300

    various cosmetic updates to man pages

--- R/games.R
+++ R/games.R
@@ -1,3 +1,6 @@
+## hawk-dove game theory model
+## written by Liam J. Revell
+
 hawk.dove<-function(p=c(0.01,0.99),M=NULL,time=100){
 	if(is.null(M)) M<-matrix(c(0.6,1.5,0.5,1.0),2,2,byrow=T)
 	if(is.null(dimnames(M))) dimnames(M)<-list(c("hawk","dove"),c("hawk","dove"))
@@ -21,9 +24,11 @@ hawk.dove<-function(p=c(0.01,0.99),M=NULL,time=100){
 	par(mar=c(5.1,4.1,1.1,2.1))
 	plot(time,dove,type="l",ylim=c(0,1),ylab="frequency",col="blue",lwd=2)
 	lines(time,hawk,type="l",col="red",lwd=2)
-	legend(max(time),1,interactors,col=c("red","blue"),bg="gray90",lwd=2,xjust=1,yjust=1)
+	legend(max(time),1,interactors,col=c("red","blue"),bg="gray90",lwd=2,
+		xjust=1,yjust=1)
 	plot(time,wbar,type="l",lwd=2,ylim=c(0,max(M)),ylab="mean fitness")
 	lines(time,wdove,type="l",col="blue",lwd=2)
 	lines(time,whawk,type="l",col="red",lwd=2)
-	legend(max(time),max(M),c("overall",interactors),col=c("black","red","blue"),bg="gray90",lwd=2,xjust=1,yjust=1)
+	legend(max(time),max(M),c("overall",interactors),col=c("black","red","blue"),
+		bg="gray90",lwd=2,xjust=1,yjust=1)
 }
