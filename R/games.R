b3eb711643817a69f484293b1b2a74d2fdf20e6f
Liam J. Revell <liam.revell@umb.edu>
2019-05-18 18:09:20 -0400

    print & plot methods for hawk.dove

--- R/games.R
+++ R/games.R
@@ -24,11 +24,47 @@ hawk.dove<-function(p=c(0.01,0.99),M=NULL,time=100){
 	par(mar=c(5.1,4.1,1.1,2.1))
 	plot(time,dove,type="l",ylim=c(0,1),ylab="frequency",col="blue",lwd=2)
 	lines(time,hawk,type="l",col="red",lwd=2)
-	legend(max(time),1,interactors,col=c("red","blue"),bg="gray90",lwd=2,
-		xjust=1,yjust=1)
+	legend(max(time),1,interactors,col=c("red","blue"),
+		bg=make.transparent("gray90",0.5),lwd=2,
+		xjust=1,yjust=1,box.col="transparent")
 	plot(time,wbar,type="l",lwd=2,ylim=c(0,max(M)),ylab="mean fitness")
 	lines(time,wdove,type="l",col="blue",lwd=2)
 	lines(time,whawk,type="l",col="red",lwd=2)
 	legend(max(time),max(M),c("overall",interactors),col=c("black","red","blue"),
-		bg="gray90",lwd=2,xjust=1,yjust=1)
+		bg=make.transparent("gray90",0.5),lwd=2,xjust=1,yjust=1,box.col="transparent")
+	object<-list(p=p,M=M,time=time[length(time)],
+		dove=dove,hawk=hawk,wbar=wbar,
+		wdove=wdove,whawk=whawk)
+	class(object)<-"hawk.dove"
+	object
+}
+
+print.hawk.dove<-function(x,...){
+	cat("\nObject of class \"hawk.dove\" consisting of the results of a")
+	cat("\nnumerical analysis of the so-called Hawk-Dove model in which")
+	cat("\nfitness is assumed to be proportional to the payoff from")
+	cat("\ninteractions between the two alternative strategies as given")
+	cat("\nby the matrix:\n")
+	print(x$M)
+	cat("\n\nTo plot enter plot(\'object_name\') at the command line\ninterface.\n\n")
+}
+
+plot.hawk.dove<-function(x,...){
+	time<-1:x$time
+	layout(c(1,2))
+	par(mar=c(5.1,4.1,1.1,2.1))
+	plot(time,x$dove,type="l",ylim=c(0,1),ylab="frequency",
+		col="blue",lwd=2)
+	lines(time,x$hawk,type="l",col="red",lwd=2)
+	legend(max(time),1,rownames(x$M),col=c("red","blue"),
+		bg=make.transparent("gray90",0.5),lwd=2,
+		xjust=1,yjust=1,box.col="transparent")
+	plot(time,x$wbar,type="l",lwd=2,ylim=c(0,max(x$M)),
+		ylab="mean fitness")
+	lines(time,x$wdove,type="l",col="blue",lwd=2)
+	lines(time,x$whawk,type="l",col="red",lwd=2)
+	legend(max(time),max(x$M),c("overall",rownames(x$M)),
+		col=c("black","red","blue"),
+		bg=make.transparent("gray90",0.5),lwd=2,xjust=1,yjust=1,
+		box.col="transparent")
 }
